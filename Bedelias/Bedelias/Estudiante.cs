64e264c6a9d8fb5f7005dd35cd118473a29bc776
Enrique González Isarrualde <54299065+EnriqueGI@users.noreply.github.com>
2022-06-01 17:43:35 -0300

    Add files via upload

---
+++ Bedelias/Bedelias/Estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bedelias
+{
+    class Estudiante: Persona
+    {
+        private int[] notas = new int[4];
+
+        public int[] Notas { get => notas; set => notas = value; }
+    }
+}
---
+++ Bedelias/Bedelias/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Bedelias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textCedula = new System.Windows.Forms.TextBox();
+            this.textEdad = new System.Windows.Forms.TextBox();
+            this.textN1 = new System.Windows.Forms.TextBox();
+            this.textN2 = new System.Windows.Forms.TextBox();
+            this.textN3 = new System.Windows.Forms.TextBox();
+            this.textN4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label1.Location = new System.Drawing.Point(133, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label2.Location = new System.Drawing.Point(133, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cédula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label3.Location = new System.Drawing.Point(133, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.PeachPuff;
+            this.label4.Location = new System.Drawing.Point(133, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Notas";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(316, 101);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(270, 26);
+            this.textNombre.TabIndex = 4;
+            // 
+            // textCedula
+            // 
+            this.textCedula.Location = new System.Drawing.Point(316, 160);
+            this.textCedula.Name = "textCedula";
+            this.textCedula.Size = new System.Drawing.Size(270, 26);
+            this.textCedula.TabIndex = 5;
+            // 
+            // textEdad
+            // 
+            this.textEdad.Location = new System.Drawing.Point(316, 220);
+            this.textEdad.Name = "textEdad";
+            this.textEdad.Size = new System.Drawing.Size(270, 26);
+            this.textEdad.TabIndex = 6;
+            // 
+            // textN1
+            // 
+            this.textN1.Location = new System.Drawing.Point(316, 284);
+            this.textN1.Name = "textN1";
+            this.textN1.Size = new System.Drawing.Size(39, 26);
+            this.textN1.TabIndex = 7;
+            // 
+            // textN2
+            // 
+            this.textN2.Location = new System.Drawing.Point(375, 285);
+            this.textN2.Name = "textN2";
+            this.textN2.Size = new System.Drawing.Size(39, 26);
+            this.textN2.TabIndex = 8;
+            // 
+            // textN3
+            // 
+            this.textN3.Location = new System.Drawing.Point(434, 284);
+            this.textN3.Name = "textN3";
+            this.textN3.Size = new System.Drawing.Size(39, 26);
+            this.textN3.TabIndex = 9;
+            // 
+            // textN4
+            // 
+            this.textN4.Location = new System.Drawing.Point(498, 284);
+            this.textN4.Name = "textN4";
+            this.textN4.Size = new System.Drawing.Size(39, 26);
+            this.textN4.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 48);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(906, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textN4);
+            this.Controls.Add(this.textN3);
+            this.Controls.Add(this.textN2);
+            this.Controls.Add(this.textN1);
+            this.Controls.Add(this.textEdad);
+            this.Controls.Add(this.textCedula);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textCedula;
+        private System.Windows.Forms.TextBox textEdad;
+        private System.Windows.Forms.TextBox textN1;
+        private System.Windows.Forms.TextBox textN2;
+        private System.Windows.Forms.TextBox textN3;
+        private System.Windows.Forms.TextBox textN4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
